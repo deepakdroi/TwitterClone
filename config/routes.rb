de726a7b6bdf2241d3b54451a68faac6b9fcafb0
deepakdroi <deepak.mahto706@gmail.com>
2019-03-15 14:54:57 +0530

    Chapter 12  - Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
-get 'sessions/new'
-get 'users/new'
-root 'static_pages#home'
-get 'home' => 'static_pages#home'
-get 'help' => 'static_pages#help'
-get 'about' => 'static_pages#about'
-get 'contact' => 'static_pages#contact'
-get 'signup' => 'users#new'
-post '/signup', to: 'users#create'
-get 'login' => 'sessions#new'
-post 'login' => 'sessions#create'
-delete 'logout' => 'sessions#destroy'
-resources :users
-resources :account_activations, only: [:edit]
+  get 'password_resets/new'
+  get 'password_resets/edit'
+  get 'sessions/new'
+  get 'users/new'
+  root 'static_pages#home'
+  get 'home' => 'static_pages#home'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup' => 'users#new'
+  post '/signup', to: 'users#create'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+
 end
